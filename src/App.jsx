161f0c1d7b4e24ae1d2b88f88f51905b0ba0bcb5
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-02-12 01:15:14 +0530

    counterapplicationusing reduxtoolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementbyAmount } from "./Reducers/Createslice";
 
 const App = () => {
-  return (
-    <div className='bg-red-600 text-center text-5xl font-bold text-white'>
-       Redux 
- 
-      
-      
-      </div>
-  )
-}
+  const { value } = useSelector((state) => state.Counter);
+  const dispatch = useDispatch();
 
-export default App
+  return (
+    <>
+      <div className="bg-red-500  p-4 w-[50%] ml-auto mr-auto flex items-center justify-center mt-32 text-center text-5xl font-bold text-white">
+        Counter : {value}
+        </div>
+        <div className=" mt-24 gap-10 w-32 flex items-center justify-center ml-auto mr-auto" >
+          
+           <button className=" bg-transparent border-2 border-slate-400 p-4" onClick={ () => dispatch(increment())}>Increment</button>
+
+           <button className=" bg-transparent border-2 border-slate-400 p-4" onClick={ () => dispatch(decrement())}>Decrement</button>
+          <button className=" bg-transparent border-2 border-slate-400 p-4" onClick={ () => dispatch(incrementbyAmount(5))}>IncrementByAmount</button>
+        
+
+
+        </div>
+     
+
+    </>
+  );
+};
+
+export default App;
--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,5 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import CounterReducer from '../Reducers/Createslice'
 
-export default configureStore({
-  reducer: {}
-})
+export const store = configureStore({
+  reducer: {
+    Counter : CounterReducer
+    
+  }
+});
