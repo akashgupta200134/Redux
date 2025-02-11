1e6fa0580a3b0f6d652be02278ebfdc9c8f30d82
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-02-11 21:47:02 +0530

    installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import Cards from "./components/Cards";
-import Form from "./components/Form";
+import React from 'react'
 
-function App() {
-  const [users, setUsers] = useState([]);
-
-  const handleSubmitdata = (data) => {
-    setUsers(() => [...users, data]); 
-  };
-
-  const handleRemove = (id) => {
-    setUsers(() => users.filter((items, index) => index !== id)); 
-  };
-
+const App = () => {
   return (
-    <>
-      <div>
-        <Cards usersdata={users} handleRemove={handleRemove} />
+    <div className='bg-red-600 text-center text-5xl font-bold text-white'>
+       Redux 
+ 
+      
+      
       </div>
-
-      <div>
-        <Form handleSubmitdata={handleSubmitdata} />
-      </div>
-    </>
-  );
+  )
 }
 
-export default App;
+export default App
